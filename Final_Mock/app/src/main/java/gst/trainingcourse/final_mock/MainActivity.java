057ed3b696099f7de60f0faf9d22c2c98ebf34a4
hiep <hiepnguyenthe262@gmail.com>
2019-09-10 06:55:33 +0700

    final_mock

---
+++ Final_Mock/app/src/main/java/gst/trainingcourse/final_mock/MainActivity.java
@@ -0,0 +1,13 @@
+package gst.trainingcourse.final_mock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
