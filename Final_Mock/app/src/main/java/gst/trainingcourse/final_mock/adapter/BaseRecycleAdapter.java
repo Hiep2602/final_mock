9ed51af02270d911ae59c00424bb74cc7ed07c91
hiep <hiepnguyenthe262@gmail.com>
2019-09-11 09:06:03 +0700

    test ignore

--- Final_Mock/app/src/main/java/gst/trainingcourse/final_mock/adapter/BaseRecycleAdapter.java
+++ Final_Mock/app/src/main/java/gst/trainingcourse/final_mock/adapter/BaseRecycleAdapter.java
@@ -1,4 +1,52 @@
 package gst.trainingcourse.final_mock.adapter;
 
-public class BaseRecycleAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+public abstract class BaseRecycleAdapter<T> extends RecyclerView.Adapter<BaseViewHolder> {
+    private List<T> mData;
+
+    public void setData(List<T> data) {
+        mData = data;
+        notifyDataSetChanged();
+    }
+
+
+    public void appendItems(List<T> data) {
+        if (data == null) return;
+        mData.addAll(data);
+        notifyItemRangeChanged(getDataSize(), data.size());
+    }
+
+    private int getDataSize() {
+        return (mData == null ? 0 : mData.size());
+    }
+
+    @NonNull
+    @Override
+    public BaseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(getViewId(), parent, false);
+        return getViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BaseViewHolder holder, int postion) {
+        onBindView(holder, mData.get(postion));
+    }
+
+    @Override
+    public int getItemCount() {
+        return getDataSize();
+    }
+
+    abstract protected int getViewId();
+
+    abstract protected BaseViewHolder getViewHolder(View view);
+
+    abstract protected void onBindView(BaseViewHolder holder, T model);
 }
