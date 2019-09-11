98c01c0a5e5577dfdb53e493136ab2c1e69068a7
hiep <hiepnguyenthe262@gmail.com>
2019-09-11 09:08:49 +0700

    ignore

--- Final_Mock/app/src/main/java/gst/trainingcourse/final_mock/adapter/BaseViewHolder.java
+++ Final_Mock/app/src/main/java/gst/trainingcourse/final_mock/adapter/BaseViewHolder.java
@@ -1,4 +1,11 @@
 package gst.trainingcourse.final_mock.adapter;
 
-public class BaseViewHolder {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class BaseViewHolder extends RecyclerView.ViewHolder {
+    public BaseViewHolder(@NonNull View view) {
+        super(view);
+    }
 }
--- Final_Mock/app/src/main/java/gst/trainingcourse/final_mock/fragment/BaseFragment.java
+++ Final_Mock/app/src/main/java/gst/trainingcourse/final_mock/fragment/BaseFragment.java
@@ -2,6 +2,6 @@ package gst.trainingcourse.final_mock.fragment;
 
 import android.support.v4.app.Fragment;
 
-public abstract class BaseFragment extends Fragment{
-
+public abstract class BaseFragment<T> extends Fragment{
+    
 }
